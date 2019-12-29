356b7785d0a97803098ac4aa83dd92280b757f7c
Luigi Asprino <luigi.asprino@gmail.com>
2019-12-29 12:25:58 +0100

    Dependency bounce

--- src/test/java/it/cnr/istc/stlab/rocksmap/test/TestUtils.java
+++ src/test/java/it/cnr/istc/stlab/rocksmap/test/TestUtils.java
@@ -1,18 +1,15 @@
 package it.cnr.istc.stlab.rocksmap.test;
 
-import java.io.File;
-import java.io.IOException;
-
 public class TestUtils {
 
-	public static final String TEST_FOLDER_PATH = "testData";
-
-	public static void clearTestFolder() {
-		try {
-			org.apache.commons.io.FileUtils.deleteDirectory(new File(TEST_FOLDER_PATH));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	public static final String TEST_FOLDER_PATH = "testData";
+//
+//	public static void clearTestFolder() {
+//		try {
+//			org.apache.commons.io.FileUtils.deleteDirectory(new File(TEST_FOLDER_PATH));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 }
