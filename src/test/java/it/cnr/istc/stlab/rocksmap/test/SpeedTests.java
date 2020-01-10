08d1cc02235ff3acf1cfb1b92645096ce5853080
Luigi Asprino <luigi.asprino@gmail.com>
2020-01-10 10:01:00 +0100

    SpeedTest added

--- src/test/java/it/cnr/istc/stlab/rocksmap/test/SpeedTests.java
+++ src/test/java/it/cnr/istc/stlab/rocksmap/test/SpeedTests.java
@@ -1,12 +1,39 @@
 package it.cnr.istc.stlab.rocksmap.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+import java.util.stream.LongStream;
+
 import org.junit.Test;
 
+import it.cnr.istc.stlab.rocksmap.transformer.LongRocksTransformer;
+import it.cnr.istc.stlab.rocksmap.transformer.LongRocksTransformerByteBuffer;
+
 public class SpeedTests {
-	
+
+	private static final long NUMBER_OF_TESTS = 100000000;
+
 	@Test
 	public void testSpeedLongTransformer() {
-		
+		LongRocksTransformer t = new LongRocksTransformer();
+		Random r = new Random(System.currentTimeMillis());
+		LongStream.range(0, NUMBER_OF_TESTS).parallel().forEach(l -> {
+			assertEquals((Long) l, t.transform(t.transform(l)));
+			Long l1 = r.nextLong();
+			assertEquals(l1, t.transform(t.transform(l1)));
+		});
+	}
+
+	@Test
+	public void testSpeedLongTransformerByteBuffer() {
+		LongRocksTransformerByteBuffer bb = new LongRocksTransformerByteBuffer();
+		Random r = new Random(System.currentTimeMillis());
+		LongStream.range(0, NUMBER_OF_TESTS).parallel().forEach(l -> {
+			assertEquals((Long) l, bb.transform(bb.transform(l)));
+			Long l1 = r.nextLong();
+			assertEquals((Long) l1, bb.transform(bb.transform(l1)));
+		});
 	}
 
 }
